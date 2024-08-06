7dc7b2bafbbc465257118a0577cc7682e912f4a3
PaperDragon <2678885646@qq.com>
2024-08-06 03:03:25 +0000

    更改 python 在 linux 获取的值是空的

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from src import os_import
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
 
 print(os_import.api_key)
 
--- src/os_import.py
+++ src/os_import.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 api_key = os.getenv('API_KEY')
 db_password = os.getenv('DB_PASSWORD')
